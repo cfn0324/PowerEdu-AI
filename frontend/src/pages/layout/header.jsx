0772ee926eafa6261d0baac3c156c9f387373613
cfn0324 <2249520721@qq.com>
2025-07-19 08:43:08 +0800

    feat: 将用户中心链接化，增强导航功能

--- frontend/src/pages/layout/header.jsx
+++ frontend/src/pages/layout/header.jsx
@@ -69,7 +69,9 @@ const Header = () => {
             <Link to="/prediction" style={{ textDecoration: 'none', color: 'inherit' }}>AI预测</Link>
           </li>
           <li>学习成就</li>
-          <li>用户中心</li>
+          <li>
+            <Link to="/profile" style={{ textDecoration: 'none', color: 'inherit' }}>用户中心</Link>
+          </li>
         </ul>
         </Link>
       </div>
